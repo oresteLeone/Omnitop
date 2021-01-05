97d71a06c80d73ce6bb32040dbb8fac6282fd93c
Marco <m.galantino@studenti.unina.it>
2021-01-05 17:25:02 +0100

    Calcolatrice Funzionante

--- app/src/main/java/absPackage/LancioDadiFragment.kt
+++ app/src/main/java/absPackage/LancioDadiFragment.kt
@@ -3,6 +3,7 @@ package absPackage
 import android.content.Context
 import android.os.Bundle
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,6 +14,9 @@ import com.annoyingturtle.omnitop.R
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.lancio_dadi_layout2.*
+import net.objecthunter.exp4j.Expression
+import net.objecthunter.exp4j.ExpressionBuilder
+import java.lang.Exception
 
 class LancioDadiFragment : BottomSheetDialogFragment() {
 
@@ -38,6 +42,11 @@ class LancioDadiFragment : BottomSheetDialogFragment() {
 
     private fun appendOnExpression(string: String) {
 
+        if(risultatoTextView.text.isNotEmpty())  {
+            displayCalcolatrice.text = risultatoTextView.text
+            risultatoTextView.text = ""
+        }
+
 
         risultatoTextView.text = ""
         displayCalcolatrice.append(string)
@@ -46,48 +55,47 @@ class LancioDadiFragment : BottomSheetDialogFragment() {
 
     fun popolaListner() {
         /****** LISTNER PER I PULSANTI *******/
-        val btn0: Button = butt0
-        val btn1: Button = butt1
-        val btn2: Button = butt2
-        val btn3: Button = butt3
-        val btn4: Button = butt4
-        val btn5: Button = butt5
-        val btn6: Button = butt6
-        val btn7: Button = butt7
-        val btn8: Button = butt8
-        val btn9: Button = butt9
-
-        val btnplus: Button = plus
-        val btnminus: Button = minus
-        val btnmultiply: Button = multiply
-        val btndivide: Button = divide
-        val btnopenParentesis: Button = openParentesis
-        val btncloseParentesis: Button = closeParentesis
-        val btnbackspace: Button = backspace
         //Numeri
 
-        btn0.setOnClickListener { appendOnExpression("0") }
-        btn1.setOnClickListener { appendOnExpression("1") }
-        btn2.setOnClickListener { appendOnExpression("2") }
-        btn3.setOnClickListener { appendOnExpression("3") }
-        btn4.setOnClickListener { appendOnExpression("4") }
-        btn5.setOnClickListener { appendOnExpression("5") }
-        btn6.setOnClickListener { appendOnExpression("6") }
-        btn7.setOnClickListener { appendOnExpression("7") }
-        btn8.setOnClickListener { appendOnExpression("8") }
-        btn9.setOnClickListener { appendOnExpression("9") }
+        butt0.setOnClickListener { appendOnExpression("0") }
+        butt1.setOnClickListener { appendOnExpression("1") }
+        butt2.setOnClickListener { appendOnExpression("2") }
+        butt3.setOnClickListener { appendOnExpression("3") }
+        butt4.setOnClickListener { appendOnExpression("4") }
+        butt5.setOnClickListener { appendOnExpression("5") }
+        butt6.setOnClickListener { appendOnExpression("6") }
+        butt7.setOnClickListener { appendOnExpression("7") }
+        butt8.setOnClickListener { appendOnExpression("8") }
+        butt9.setOnClickListener { appendOnExpression("9") }
 
         //Operatori
 
-        btnplus.setOnClickListener { appendOnExpression("+") }
-        btnminus.setOnClickListener { appendOnExpression("-") }
-        btnmultiply.setOnClickListener { appendOnExpression("×") }
-        btndivide.setOnClickListener { appendOnExpression("÷") }
-        btnopenParentesis.setOnClickListener { appendOnExpression("(") }
-        btncloseParentesis.setOnClickListener { appendOnExpression(")") }
+        plus.setOnClickListener { appendOnExpression("+") }
+        minus.setOnClickListener { appendOnExpression("-") }
+        multiply.setOnClickListener { appendOnExpression("*") }
+        divide.setOnClickListener { appendOnExpression("/") }
+        openParentesis.setOnClickListener { appendOnExpression("(") }
+        closeParentesis.setOnClickListener { appendOnExpression(")") }
+
+        //Pulsante Roll
+
+        buttRoll.setOnClickListener(){
+            try{
+
+                var espressione = ExpressionBuilder(displayCalcolatrice.text.toString()).build()
+                var result = espressione.evaluate()
+                var longResult = result.toLong()
+                if(result == longResult.toDouble()) risultatoTextView.text = longResult.toString()
+                    else
+                    risultatoTextView.text = result.toString()
+
+            }catch (e : Exception){
+                Log.d("Eccezione Calcolatrice", "message:" + e.message)
+            }
+        }
 
 
-        btnbackspace.setOnClickListener {
+        backspace.setOnClickListener {
             val string = displayCalcolatrice.text.toString()
             if (string.isNotEmpty()) {
                 displayCalcolatrice.text = string.substring(0, string.length - 1)
