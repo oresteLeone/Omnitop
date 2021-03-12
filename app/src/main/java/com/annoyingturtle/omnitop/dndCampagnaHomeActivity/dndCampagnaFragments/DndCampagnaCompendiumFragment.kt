70705b701f2d2f06d8164e99928196bbfbf7e8e8
Marco <m.galantino@studenti.unina.it>
2021-03-12 11:59:56 +0100

    Roba

--- app/src/main/java/com/annoyingturtle/omnitop/dndCampagnaHomeActivity/dndCampagnaFragments/DndCampagnaCompendiumFragment.kt
+++ app/src/main/java/com/annoyingturtle/omnitop/dndCampagnaHomeActivity/dndCampagnaFragments/DndCampagnaCompendiumFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.annoyingturtle.omnitop.R
+import kotlinx.android.synthetic.main.fragment_dnd_campagna_compendium.*
 
 
 class DndCampagnaCompendiumFragment : Fragment() {
@@ -16,6 +18,22 @@ class DndCampagnaCompendiumFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dnd_campagna_compendium, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        imageViewEquipaggiamento.setOnClickListener(){
+            Toast.makeText(requireContext(), "Pulsante Equipaggiamento", Toast.LENGTH_SHORT).show()
+        }
+
+        imageViewBestiario.setOnClickListener(){
+            Toast.makeText(requireContext(), "Pulsante Bestiario", Toast.LENGTH_SHORT).show()
+        }
+
+        imageViewIncantesimi.setOnClickListener(){
+            Toast.makeText(requireContext(), "Pulsante Incantesimi", Toast.LENGTH_SHORT).show()
+        }
     }
 
 
